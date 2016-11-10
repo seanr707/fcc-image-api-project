b595888eca355f5f55282105cdfe6ee23fe4c528
Ankur Sharma <restlessankur@gmail.com>
2016-11-11 00:31:01 +0530

    4th commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var multer  = require('multer');
 var request = require('request');
 
-var Bing = require('node-bing-api-master')
+var Bing = require('node-bing-api')
             ({
               accKey: "7e792dc10eed41b9a079f6d1a30a97bd"
 
@@ -17,73 +17,36 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 app.listen(process.env.PORT || 5000);
 
-/*
-app.get("/",function(req,res){
-  res.render("\views\index.html")
-});
-*/
 
-  /*jquery code starts
-  $( document ).ready(function() {
-    var params = {
-        // Request parameters
-        "q": "cats",
-    "count": "30",
-    "offset": "0",
-    "mkt": "en-us",
-    "safeSearch": "Moderate",
-    };
-
-//used in other file
-    $.ajax({
-        url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(params),
-        beforeSend: function(xhrObj){
-            // Request headers
-            xhrObj.setRequestHeader("Content-Type","multipart/form-data");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","7e792dc10eed41b9a079f6d1a30a97bd");
-        },
-        type: "POST",
-        // Request body
-        data: "{body}",
-    })
-    .done(function(data) {
-        //data.value
-         var fdata ={};
-         console.log(data);
-        for(var i=0;i<29;i++){
-          var tg = data.value[i].contentUrl;
-          fdata[i]=tg;
-        }
-        console.log(data);
-        console.log(fdata);
-
-    })
-    .fail(function() {
-        alert("error");
-    });
-
-
-//using end
-
-      console.log( "ready!" );
-  });
-
-
-
-
-
-  //jquery code ends
-  */
-  app.get("/api/imagesearch/:id",function(req,res){
+app.get("/api/imagesearch/:id",function(req,response){
 
       var f = req.params.id;
       var t = req.query.offset;
+      var g;
+      var k;
+      var  l;
+      var tarray =[];
       console.log("this is f ",f,"this is t ",t)
-    Bing.images(f, {skip: 50}, function(error, res, body){
+    Bing.images(f, {top:40, skip: 0}, function(error, res, body){
       console.log("started");
-      var g = JSON.stringify(body);
+      g = JSON.stringify(body);
+
+      k=body;
+      for(var i =0;i<40;i++)
+      {
+        tarray.push({
+        url: body.value[i].contentUrl,
+        snippet: body.value[i].name,
+        thumbnail: body.value[i].thumbnailUrl,
+        context:body.value[i].hostPageDisplayUrl
+    });
+
+      }
       console.log(g);
       console.log("end");
+     l =JSON.stringify(tarray)
+
+     response.json(l);
     });
 
   });
@@ -91,79 +54,3 @@ app.get("/",function(req,res){
 app.get("/",function(req,res){
   res.render("index.html");
 })
-
-
-
-
-
-
-
-
-/*
-
-app.get("/api/imagesearch/:query",function(req,res){
-console.log("came here");
-  var query = req.params.query;
-  var size = req.query.offset;
-  var options = {
-    url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + query,
-    beforeSend: function(xhrObj){
-        // Request headers
-        xhrObj.setRequestHeader("Content-Type","multipart/form-data");
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","7e792dc10eed41b9a079f6d1a30a97bd");
-
-    },
-    type: "POST",
-    // Request body
-    data: "{body}",
-    /*
-    headers: {
-      'User-Agent': 'request'
-    }
-
-  };
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      console.log(info);
-    }
-  }
-
-  request(options, callback);
-
-
-
-
-      $.ajax({
-          url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + query,
-          beforeSend: function(xhrObj){
-              // Request headers
-              xhrObj.setRequestHeader("Content-Type","multipart/form-data");
-              xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","7e792dc10eed41b9a079f6d1a30a97bd");
-          },
-          type: "POST",
-          // Request body
-          data: "{body}",
-      })
-      .done(function(data) {
-          //data.value
-           var fdata ={};
-           console.log(data);
-          for(var i=0;i<29;i++){
-            var tg = data.value[i].contentUrl;
-            fdata[i]=tg;
-          }
-          console.log(data);
-          console.log(fdata);
-
-      })
-      .fail(function() {
-          alert("error");
-          console.log("there is error");
-      });
-
-
-
-})
-*/
