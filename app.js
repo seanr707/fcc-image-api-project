75d57eef4c43106ec555f810a46eec33c8890b33
Ankur Sharma <restlessankur@gmail.com>
2016-11-13 22:26:38 +0530

    6th commit is

--- app.js
+++ app.js
@@ -19,37 +19,89 @@ app.listen(process.env.PORT || 5000);
 
 
 app.get("/api/imagesearch/:id",function(req,response){
-
       var f = req.params.id;
       var t = req.query.offset;
+      console.log("value of offset ",t);
       var g;
       var k;
       var  l;
       var tarray =[];
-      console.log("this is f ",f,"this is t ",t)
-    Bing.images(f, {top:40, skip: 0}, function(error, res, body){
-      console.log("started");
-      g = JSON.stringify(body);
 
-      k=body;
-      for(var i =0;i<40;i++)
-      {
-        tarray.push({
-        url: body.value[i].contentUrl,
-        snippet: body.value[i].name,
-        thumbnail: body.value[i].thumbnailUrl,
-        context:body.value[i].hostPageDisplayUrl
-    });
+      // start
 
+      if(t===1){
+        Bing.images(f, {top:10, skip: 0}, function(error, res, body){
+          console.log("started");
+          g = JSON.stringify(body);
+          for(var i =0;i<9;i++)
+          {
+            tarray.push({
+            url: body.value[i].contentUrl,
+            snippet: body.value[i].name,
+            thumbnail: body.value[i].thumbnailUrl,
+            context:body.value[i].hostPageDisplayUrl
+                        });
+
+          }
+
+          response.json(tarray);
+          console.log(tarray);
+        })
       }
-      console.log(g);
-      console.log("end");
-     l =JSON.stringify(tarray)
 
-     response.json(l);
-    });
+        else if (t===2){
+          Bing.images(f, {top:20, skip: 10}, function(error, res, body){
+            console.log("started");
+            g = JSON.stringify(body);
+            for(var i =0;i<9;i++)
+            {
+              tarray.push({
+              url: body.value[i].contentUrl,
+              snippet: body.value[i].name,
+              thumbnail: body.value[i].thumbnailUrl,
+              context:body.value[i].hostPageDisplayUrl
+          });
 
-  });
+            }
+            response.json(tarray);
+          })
+
+        }
+
+        else if(t===3){
+          Bing.images(f, {top:30, skip: 20}, function(error, res, body){
+            console.log("started");
+            g = JSON.stringify(body);
+            for(var i =0;i<9;i++)
+            {
+              tarray.push({
+              url: body.value[i].contentUrl,
+              snippet: body.value[i].name,
+              thumbnail: body.value[i].thumbnailUrl,
+              context:body.value[i].hostPageDisplayUrl
+          });
+
+            }
+            response.json(tarray);
+          })
+
+        }
+        else{
+          response.send("No results to display. Please put offset=1,2 or 3. Api doesnt understand your offset value")
+        }
+
+
+       console.log(tarray);
+
+
+      })
+
+
+
+      //end
+
+
+
 
 app.get("/",function(req,res){
   res.render("index.html");
