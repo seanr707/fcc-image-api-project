da8f0a284cbc7b7a186b131af65f4c6980bee182
seanr707 <seanr.git@gmail.com>
2016-11-14 09:00:54 -0500

    Fixed some of the codes formatting issues

--- app.js
+++ app.js
@@ -1,108 +1,94 @@
-var express = require('express');
+const express = require('express');
 
-var path = require('path');
-var app = express();
-var multer  = require('multer');
-var request = require('request');
+const path = require('path');
+const app = express();
+const multer  = require('multer');
+const request = require('request');
 
-var Bing = require('node-bing-api')
-            ({
-              accKey: "7e792dc10eed41b9a079f6d1a30a97bd"
+const Bing = require('node-bing-api')({
+  accKey: '7e792dc10eed41b9a079f6d1a30a97bd'
+});
 
-            });
-var upload = multer({ dest: 'uploads/' });
-var t;
+const upload = multer({ dest: 'uploads/' });
+
+const sendBingResults = (bingApi, id, page, callback) => {
+  const top = page * 10;
+  const skip = 10 * page - 10;
+  bingApi.images(id, { top, skip }, callback);
+};
 
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.listen(process.env.PORT || 5000);
 
 
-app.get("/api/imagesearch/:id",function(req,response){
-      var f = req.params.id;
-      var t = req.query.offset;
-      console.log("value of offset ",t);
-      var g;
-      var k;
-      var  l;
-      var tarray =[];
+app.get('/api/imagesearch/:id', (req, res) => {
+  const f = req.params.id;
+  const t = req.query.offset;
+  console.log('value of offset ' + page);
 
-      // start
+  let arr = [];
 
-      if(t===1){
-        Bing.images(f, {top:10, skip: 0}, function(error, res, body){
-          console.log("started");
-          g = JSON.stringify(body);
-          for(var i =0;i<9;i++)
-          {
-            tarray.push({
-            url: body.value[i].contentUrl,
-            snippet: body.value[i].name,
-            thumbnail: body.value[i].thumbnailUrl,
-            context:body.value[i].hostPageDisplayUrl
-                        });
+  // start
 
-          }
-
-          response.json(tarray);
-          console.log(tarray);
-        })
+  if (t===1) {
+    Bing.images(f, {top:10, skip: 0}, (error, res, body) => {
+      console.log('started');
+      for (const i =0; i<9; i++) {
+        arr = arr.concat({
+          url: body.value[i].contentUrl,
+          snippet: body.value[i].name,
+          thumbnail: body.value[i].thumbnailUrl,
+          context:body.value[i].hostPageDisplayUrl
+        });
       }
+      console.log(arr);
+      res.json(arr);
+    })
+  }
 
-        else if (t===2){
-          Bing.images(f, {top:20, skip: 10}, function(error, res, body){
-            console.log("started");
-            g = JSON.stringify(body);
-            for(var i =0;i<9;i++)
-            {
-              tarray.push({
-              url: body.value[i].contentUrl,
-              snippet: body.value[i].name,
-              thumbnail: body.value[i].thumbnailUrl,
-              context:body.value[i].hostPageDisplayUrl
-          });
-
-            }
-            response.json(tarray);
-          })
-
-        }
-
-        else if(t===3){
-          Bing.images(f, {top:30, skip: 20}, function(error, res, body){
-            console.log("started");
-            g = JSON.stringify(body);
-            for(var i =0;i<9;i++)
-            {
-              tarray.push({
-              url: body.value[i].contentUrl,
-              snippet: body.value[i].name,
-              thumbnail: body.value[i].thumbnailUrl,
-              context:body.value[i].hostPageDisplayUrl
-          });
-
-            }
-            response.json(tarray);
-          })
+    else if (t===2){
+      Bing.images(f, {top:20, skip: 10}, function(error, res, body){
+        console.log('started');
+        g = JSON.stringify(body);
+        for(const i =0;i<9;i++)
+        {
+          tarray.push({
+          url: body.value[i].contentUrl,
+          snippet: body.value[i].name,
+          thumbnail: body.value[i].thumbnailUrl,
+          context:body.value[i].hostPageDisplayUrl
+      });
 
         }
-        else{
-          response.send("No results to display. Please put offset=1,2 or 3. Api doesnt understand your offset value")
-        }
-
-
-       console.log(tarray);
-
-
+        response.json(tarray);
       })
 
+    }
 
+    else if(t===3){
+      Bing.images(f, {top:30, skip: 20}, function(error, res, body){
+        console.log('started');
+        g = JSON.stringify(body);
+        for(const i =0;i<9;i++)
+        {
+          tarray.push({
+          url: body.value[i].contentUrl,
+          snippet: body.value[i].name,
+          thumbnail: body.value[i].thumbnailUrl,
+          context:body.value[i].hostPageDisplayUrl
+      });
 
-      //end
+        }
+        response.json(tarray);
+      })
 
+    }
+    else{
+      response.send('No results to display. Please put offset=1,2 or 3. Api doesnt understand your offset value')
+    }
+});
 
-
-
-app.get("/",function(req,res){
-  res.render("index.html");
+app.get('/', (req, res) => {
+  res.render('index.html');
 })
